7720bfc38c20f77ec06950cf9e91a700245fc68a
Phlake <marko6462@gmail.com>
2022-06-17 14:40:29 +0300

    minor changes

--- 8.sql
+++ 8.sql
@@ -1,4 +1,6 @@
-SELECT ship, 
-	(SELECT classes.country FROM ships JOIN classes ON outcomes.ship = ships.name AND ships.class = classes.class)  AS country
+SELECT ship,
+	(SELECT classes.country FROM ships JOIN classes ON
+	    outcomes.ship = ships.name AND ships.class = classes.class)  AS country
 FROM outcomes
-WHERE result='sunk' AND (SELECT ships.name FROM ships WHERE outcomes.ship = ships.name) IS NOT NULL;
+WHERE result='sunk' AND
+      (SELECT ships.name FROM ships WHERE outcomes.ship = ships.name) IS NOT NULL;
